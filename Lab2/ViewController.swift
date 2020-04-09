64372eb56e560c0012c7f5d30abd554ad37ad1c8
knzee <knzee@mail.ru>
2020-04-08 22:10:21 -0700

    Initial Commit

---
+++ Lab2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lab2
+//
+//  Created by Test Testovich on 08/04/2020.
+//  Copyright © 2020 TSU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
